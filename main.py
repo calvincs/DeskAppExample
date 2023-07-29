b2cfed1b731c276212486d18e3fce5fde0619ad7
calvincs <calvin@ventibean.com>
2023-07-29 14:33:41 -0500

    init migration to pywebview

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import logging
+from logging.handlers import RotatingFileHandler
 from contextlib import redirect_stdout
 from io import StringIO
-
+import os
 from server import server
-
 import webview
 
-logger = logging.getLogger(__name__)
-
+# flake8: disable=E501
 
 if __name__ == '__main__':
+    # Logs Path
+    logs_dir = os.path.join(os.path.dirname(__file__), 'logs')
+    server.logger.setLevel(logging.INFO) # Set the minimum log level of interest
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)  # create the directory if it does not exist
+    log_file = os.path.join(logs_dir, "application.log")
+    handler = RotatingFileHandler(log_file, maxBytes=10240, backupCount=5)  # 10 KB file size, keep 10 backup logs
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    server.log_file_name = log_file
+    server.logger.setLevel(logging.INFO)
+    server.logger.addHandler(handler)
+    server.logger.info("Application started")
+
+    # Start the application server
     stream = StringIO()
     with redirect_stdout(stream):
         window = webview.create_window('My first pywebview application', server)
-        webview.start(debug=True)
+        webview.start(debug=False)
--- server.py
+++ server.py
@@ -2,61 +2,74 @@
 
 
 from flask import Flask, render_template, request
-import webbrowser
-from threading import Timer, Thread
-import time
+from functools import wraps
+import webview
 import os
-import signal
+import json 
+import time
 import psutil
 import configparser
 import random
-import logging
-from logging.handlers import RotatingFileHandler
-
-app = Flask(__name__)
-LAST_PING = time.time()
-
-app.logger.setLevel(logging.INFO) # Set the minimum log level of interest
-log_dir = "logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)  # create the directory if it does not exist
-
-log_file = os.path.join(log_dir, "app.log")
-handler = RotatingFileHandler(log_file, maxBytes=10240, backupCount=5)  # 10 KB file size, keep 10 backup logs
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-app.logger.setLevel(logging.INFO)
-app.logger.addHandler(handler)
-
-REDIRECT_URL = "http://127.0.0.1:5000/home"
 
 
-# No Caching Allowed
-@app.after_request
+# Setup the Flask Server
+gui_dir = os.path.join(os.path.dirname(__file__), 'gui')
+
+if not os.path.exists(gui_dir):
+    gui_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'gui')
+
+templates_dir = os.path.join(gui_dir, 'templates')
+static_dir = os.path.join(gui_dir, 'static')
+
+server = Flask(__name__, static_folder=static_dir, template_folder=templates_dir)
+server.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1  # disable caching
+
+
+def verify_token(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        data = json.loads(request.data)
+        token = data.get('token')
+        if token == webview.token:
+            return function(*args, **kwargs)
+        else:
+            raise Exception('Authentication error')
+
+    return wrapper
+
+
+@server.after_request
 def add_header(response):
-    """
-        Add headers to both force latest IE rendering engine or Chrome Frame,
-        and also to cache the rendered page for 10 minutes.
-    """
-    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '-1'
+    response.headers['Cache-Control'] = 'no-store'
     return response
 
+
+@server.route('/')
+def landing():
+    """
+    Render index.html. Initialization is performed asynchronously in initialize() function
+    """
+    return render_template('index.html', token=webview.token)
+
+
+@server.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store'
+    return response
+
+
+
 # Web Routes
-@app.route('/')
+@server.route('/')
 def index():
     """
         This function renders the index.html template when you visit the root URL.
     """
-    global REDIRECT_URL
-
-    app.logger.info("Index page visited")
-    return render_template('index.html', on_exit=REDIRECT_URL)
+    server.logger.info("Index page visited")
+    return render_template('index.html')
 
 
-@app.route("/home")
+@server.route("/home")
 def home():
     """
         This function renders the home.html template when you visit the /home URL.
@@ -73,7 +86,7 @@ def home():
     return render_template("welcome.html", data=data)
 
 
-@app.route('/calc', methods=['GET', 'POST'])
+@server.route('/calc', methods=['GET', 'POST'])
 def calculate():
     """
         This function renders the calculator.html template when you visit the /calc URL.
@@ -93,11 +106,11 @@ def calculate():
             elif operation == 'divide':
                 result = num1 / num2
 
-            app.logger.info("Calculation performed: {} {} {} = {}".format(num1, operation, num2, result))
+            server.logger.info("Calculation performed: {} {} {} = {}".format(num1, operation, num2, result))
 
             return render_template('calculator.html', result=str(result))
         except Exception as e:
-            app.logger.error("Error occured: {}".format(str(e)))
+            server.logger.error("Error occured: {}".format(str(e)))
 
             if str(e) == "division by zero":
                 return "You can't divide by zero", 400
@@ -107,7 +120,7 @@ def calculate():
     return render_template('calculator.html')
 
 
-@app.route("/system")
+@server.route("/system")
 def system_info():
     """
         This function renders the system_info.html template when you visit the /system URL.
@@ -126,13 +139,13 @@ def system_info():
     # Get Disk info
     disk_info = psutil.disk_usage('/')
 
-    app.logger.info("System info page visited")
+    server.logger.info("System info page visited")
 
     return render_template('system_info.html', processes=processes, memory_info=memory_info, cpu_info=cpu_info, disk_info=disk_info)
 
 
 # Route to display system configuration using the configuration.html template
-@app.route("/system_configuration", methods=["GET", "POST"])
+@server.route("/system_configuration", methods=["GET", "POST"])
 def system_configuration():
     """
         This function renders the configuration.html template when you visit the /system_configuration URL.
@@ -145,26 +158,22 @@ def system_configuration():
     if request.method == "GET":
         # Access values from the config.ini file
         app_name = config.get('APP', 'name')
-        on_exit = config.get('APP', 'on_exit')
         owner = config.get('APP', 'owner')
         version = config.get('APP', 'version')
         created = config.get('APP', 'created')
         return render_template('configuration.html', **{
-            'app_name': config['APP']['name'],
-            'on_exit': config['APP']['on_exit'],
-            'owner': config['APP']['owner'],
-            'version': config['APP']['version'],
-            'created': config['APP']['created'],
+            'app_name': app_name,
+            'owner': owner,
+            'version': version,
+            'created': created,
         })    
     
     elif request.method == "POST":
         # Update the config values with the submitted form data
         config['APP']['name'] = request.form['name']
-        config['APP']['on_exit'] = request.form['on_exit']
         config['APP']['owner'] = request.form['owner']
         config['APP']['version'] = request.form['version']
         config['APP']['created'] = request.form['created']
-
 
         #Simulate delay to show off loader, and simulate a failed update.
         random_delay = random.randint(1, 5)
@@ -174,11 +183,10 @@ def system_configuration():
         if random.randint(0, 1) == 0:
             update_status = "Config update failed!"
 
-            app.logger.warning("Config update failed!")
+            server.logger.warning("Config update failed!")
 
             return render_template('configuration.html', **{
                 'app_name': config['APP']['name'],
-                'on_exit': config['APP']['on_exit'],
                 'owner': config['APP']['owner'],
                 'version': config['APP']['version'],
                 'created': config['APP']['created'],
@@ -189,13 +197,12 @@ def system_configuration():
             # Write the changes back to the config.ini file
             write_config(config)
 
-            app.logger.info("Config updated successfully!")
+            server.logger.info("Config updated successfully!")
 
             # Prepare the update status message
             update_status = "Config updated successfully!"
             return render_template('configuration.html', **{
                 'app_name': config['APP']['name'],
-                'on_exit': config['APP']['on_exit'],
                 'owner': config['APP']['owner'],
                 'version': config['APP']['version'],
                 'created': config['APP']['created'],
@@ -206,72 +213,16 @@ def system_configuration():
         return "Method not allowed", 405
 
 
-@app.route("/logs")
+@server.route("/logs")
 def logs():
     """
         This function renders the logs.html template when you visit the /logs URL.
     """
-    with open('logs/app.log', 'r') as f:
+    with open(server.log_file_name, 'r') as f:
         lines = f.readlines()
         last_lines = [line.strip() for line in lines[-10:]]
         
     return render_template("logs.html", logs=last_lines)
-
-
-
-@app.route('/exit')
-def exit_app():
-    """
-        This function redirects the user to Google and shuts down the Flask server.
-        Effectively, this function exits the application.
-    """
-    app.logger.info("Exit page visited")
-
-    Thread(target=shutdown_server).start()
-    return "goodbye", 200
-
-
-@app.route('/ping', methods=['POST'])
-def ping():
-    """
-        This function is called by the client to 'ping' the server.
-        It is used to monitor the connection between the client and server.
-    """
-    global LAST_PING
-    LAST_PING = time.time()  # update the time of the last received 'ping'
-    return "pong", 200
-
-
-
-# Helper Functions
-def open_browser():
-    """
-        This function opens the default web browser to the Flask server's URL.
-    """
-    app.logger.info("Opening browser to main application page")
-    webbrowser.open_new('http://127.0.0.1:5000/')
-
-
-def shutdown_server():
-    """
-        This function stops the Flask server after a delay.
-    """
-    app.logger.info("Shutting down server")
-    time.sleep(0.25)
-    os.kill(os.getpid(), signal.SIGINT)
-
-
-def monitor_pings():
-    """
-        This function monitors the time between 'pings' from the client.
-        If the client exits without sending a 'ping', the server will be stopped after some time.
-    """
-    global LAST_PING
-    while True:
-        time.sleep(5)
-        if time.time() - LAST_PING > 10:  # if more than 10 seconds passed since the last ping, stop the server
-            app.logger.warning('No ping received in the last 10 seconds, stopping server.')
-            shutdown_server()
 
 
 # Function to read the config.ini file
@@ -283,6 +234,7 @@ def read_config():
     config.read('config.ini')
     return config
 
+
 # Function to write changes to the config.ini file
 def write_config(config_data):
     """
@@ -290,28 +242,5 @@ def write_config(config_data):
         It also updates the global state variables with the new values.
     """
     # Update the config.ini file with the new values, but also global state variables
-    global REDIRECT_URL
-    REDIRECT_URL = config['APP']['on_exit']
-
     with open('config.ini', 'w') as config_file:
         config_data.write(config_file)
-
-
-if __name__ == "__main__":
-    """
-        This is the main function that is executed when you run the script.
-    """
-    # Start the ping monitoring in a separate thread
-    Thread(target=monitor_pings, daemon=True).start()
-    # Open the web browser after 1 second, giving the app time to start up
-    Timer(1.25, open_browser).start()
-
-    # Setup some variables to be used in the app
-    config = read_config()
-
-    if "on_exit" in config['APP']:
-        REDIRECT_URL = config['APP']['on_exit']
-
-    # If debug is True, and use_reloader is True, then the server will be restarted when code changes
-    app.run(use_reloader=False, debug=False)
-
